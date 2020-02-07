d4995651186a76006afd334068b105d88ebfa9d2
Jaime E. Valdez <jvaldezch@gmail.com>
2020-02-07 09:54:57 -0600

    Tráficos multiples.

--- application/modules/trafico/models/TraficosTmp.php
+++ application/modules/trafico/models/TraficosTmp.php
@@ -1,97 +1,81 @@
 <?php
 
-class Trafico_Model_TraficosTmp {
+class Trafico_Model_TraficosTmp
+{
 
     protected $_db_table;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->_db_table = new Trafico_Model_DbTable_TraficosTmp();
     }
 
-    public function verificar($idAduana, $pedimento) {
-        try {
-            $sql = $this->_db_table->select()
-                    ->from($this->_db_table, array("id"))
-                    ->where("idAduana = ?", $idAduana)
-                    ->where("pedimento = ?", $pedimento);
-            $stmt = $this->_db_table->fetchRow($sql);
-            if ($stmt) {
-                return $stmt["id"];
-            }
-            return;
-        } catch (Zend_Db_Adapter_Exception $e) {
-            throw new Exception("DB Exception found on " . __METHOD__ . ": " . $e->getMessage());
+    public function verificar($idAduana, $pedimento)
+    {
+        $sql = $this->_db_table->select()
+            ->from($this->_db_table, array("id"))
+            ->where("idAduana = ?", $idAduana)
+            ->where("pedimento = ?", $pedimento);
+        $stmt = $this->_db_table->fetchRow($sql);
+        if ($stmt) {
+            return $stmt["id"];
         }
+        return null;
     }
 
-    public function obtener($usuario) {
-        try {
-            $sql = $this->_db_table->select()
-                    ->setIntegrityCheck(false)
-                    ->from(array("t" => "traficos_tmp"), array("*"))
-                    ->joinLeft(array("c" => "trafico_clientes"), "t.idCliente = c.id", array("nombre"))
-                    ->where("t.usuario = ?", $usuario);
-            $stmt = $this->_db_table->fetchAll($sql);
-            if ($stmt) {
-                return $stmt->toArray();
-            }
-            return;
-        } catch (Zend_Db_Adapter_Exception $e) {
-            throw new Exception("DB Exception found on " . __METHOD__ . ": " . $e->getMessage());
+    public function obtener($usuario)
+    {
+        $sql = $this->_db_table->select()
+            ->setIntegrityCheck(false)
+            ->from(array("t" => "traficos_tmp"), array("*"))
+            ->joinLeft(array("c" => "trafico_clientes"), "t.idCliente = c.id", array("nombre"))
+            ->where("t.usuario = ?", $usuario);
+        $stmt = $this->_db_table->fetchAll($sql);
+        if ($stmt) {
+            return $stmt->toArray();
         }
+        return null;
     }
 
-    public function agregar($arr) {
-        try {
-            $stmt = $this->_db_table->insert($arr);
-            if ($stmt) {
-                return $stmt;
-            }
-            return;
-        } catch (Zend_Db_Adapter_Exception $e) {
-            throw new Exception("DB Exception found on " . __METHOD__ . ": " . $e->getMessage());
+    public function agregar($arr)
+    {
+        $stmt = $this->_db_table->insert($arr);
+        if ($stmt) {
+            return $stmt;
         }
+        return null;
     }
 
-    public function actualizar($id, $arr) {
-        try {
-            $stmt = $this->_db_table->update($arr, array("id = ?" => $id));
-            if ($stmt) {
-                return true;
-            }
-            return;
-        } catch (Zend_Db_Adapter_Exception $e) {
-            throw new Exception("DB Exception found on " . __METHOD__ . ": " . $e->getMessage());
+    public function actualizar($id, $arr)
+    {
+        $stmt = $this->_db_table->update($arr, array("id = ?" => $id));
+        if ($stmt) {
+            return true;
         }
+        return null;
     }
 
-    public function borrar($id) {
-        try {
-            $stmt = $this->_db_table->delete(array("id = ?" => $id));
-            if ($stmt) {
-                return true;
-            }
-            return;
-        } catch (Zend_Db_Adapter_Exception $e) {
-            throw new Exception("DB Exception found on " . __METHOD__ . ": " . $e->getMessage());
+    public function borrar($id)
+    {
+        $stmt = $this->_db_table->delete(array("id = ?" => $id));
+        if ($stmt) {
+            return true;
         }
+        return null;
     }
 
-    public function seleccionar($id) {
-        try {
-            $sql = $this->_db_table->select()
-                    ->setIntegrityCheck(false)
-                    ->from(array("t" => "traficos_tmp"), array("*"))
-                    ->joinLeft(array("c" => "trafico_clientes"), "t.idCliente = c.id", array("nombre"))
-                    ->where("t.id = ?", $id);
-            $stmt = $this->_db_table->fetchRow($sql);
-            if ($stmt) {
-                return $stmt->toArray();
-            }
-            return;
-        } catch (Zend_Db_Adapter_Exception $e) {
-            throw new Exception("DB Exception found on " . __METHOD__ . ": " . $e->getMessage());
+    public function seleccionar($id)
+    {
+        $sql = $this->_db_table->select()
+            ->setIntegrityCheck(false)
+            ->from(array("t" => "traficos_tmp"), array("*"))
+            ->joinLeft(array("c" => "trafico_clientes"), "t.idCliente = c.id", array("nombre"))
+            ->where("t.id = ?", $id);
+        $stmt = $this->_db_table->fetchRow($sql);
+        if ($stmt) {
+            return $stmt->toArray();
         }
+        return null;
     }
 
 }
